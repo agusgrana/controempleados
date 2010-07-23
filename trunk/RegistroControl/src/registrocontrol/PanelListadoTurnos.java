fdb1dcc7589ef4935a558861cf64cb3464479f9f
gurzaf <gurzaf@206f965f-f5fc-8524-13fd-0742c8e6e893>
2010-07-23 21:35:10 +0000

    Listo el Panel de Turnos y listado de empleados por turnos

--- trunk/RegistroControl/src/registrocontrol/PanelListadoTurnos.java
+++ trunk/RegistroControl/src/registrocontrol/PanelListadoTurnos.java
@@ -209,7 +209,8 @@ public class PanelListadoTurnos extends javax.swing.JPanel {
 
     private void jTable1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable1MouseClicked
            if(evt.getClickCount()>=2){
-                
+                Turnosvista t = turnosList.get(jTable1.getSelectedRow());
+                registroControlView.cambiarPanelPrincipal(new PanelListadoTurnosEmpleados(registroControlView, t.getHorario(), t.getFecha()));
            }
     }//GEN-LAST:event_jTable1MouseClicked
 
